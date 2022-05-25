1402992178541b9c4a347eabe3dcd7ee0a2b6e71
ttbinh139 <ttbinh139@gmail.com>
2022-05-25 23:46:33 +0000

    Create tweet html output template

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,78 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const createTweetElement = function (tweet) {
+  let outputHtml = `<article class="tweet">
+                    <header>
+                      <div class="tweet-owner">
+                        <img src="${tweet.user.avatars}">
+                        <span>${tweet.user.name}</span>
+                      </div>
+                      <div class="tweet-username">
+                        <span>${tweet.user.handle}</span>
+                      </div>
+                    </header>
+                    <div class="tweet-body">${tweet.content.text}</div>
+                    <footer>
+                      <div class="footer-left">
+                        <span>${tweet.created_at}</span>
+                      </div>
+                      <div class="footer-right">
+                        <i class="fa-solid fa-flag"></i>
+                        <i class="fa-solid fa-arrows-rotate"></i>
+                        <i class="fa-solid fa-heart"></i>
+                      </div>
+                    </footer>
+                  </article>`;
+  return outputHtml;
+}
+
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    let outputTweetHtml = createTweetElement(tweet);
+    $("#tweet-container").append(outputTweetHtml);
+  }
+}
+
+$(document).ready(function() {
+  renderTweets(data);
+})
+
+
