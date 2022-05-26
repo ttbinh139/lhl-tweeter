c79d8cf4482fde0fab9c55eca6486cee63091a2e
ttbinh139 <ttbinh139@gmail.com>
2022-05-26 02:40:34 +0000

    Fetch tweets and implement timeago to format time

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,67 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const createTweetElement = function (tweet) {
-  let outputHtml = `<article class="tweet">
-                    <header>
-                      <div class="tweet-owner">
-                        <img src="${tweet.user.avatars}">
-                        <span>${tweet.user.name}</span>
-                      </div>
-                      <div class="tweet-username">
-                        <span>${tweet.user.handle}</span>
-                      </div>
-                    </header>
-                    <div class="tweet-body">${tweet.content.text}</div>
-                    <footer>
-                      <div class="footer-left">
-                        <span>${tweet.created_at}</span>
-                      </div>
-                      <div class="footer-right">
-                        <i class="fa-solid fa-flag"></i>
-                        <i class="fa-solid fa-arrows-rotate"></i>
-                        <i class="fa-solid fa-heart"></i>
-                      </div>
-                    </footer>
-                  </article>`;
+  let outputHtml = 
+    `<article class="tweet">
+      <header>
+        <div class="tweet-owner">
+          <img src="${tweet.user.avatars}">
+          <span>${tweet.user.name}</span>
+        </div>
+        <div class="tweet-username">
+          <span>${tweet.user.handle}</span>
+        </div>
+      </header>
+      <div class="tweet-body">${tweet.content.text}</div>
+      <footer>
+        <div class="footer-left">
+          <span class="timeago">${timeago.format(tweet.created_at)}</span>
+        </div>
+        <div class="footer-right">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-arrows-rotate"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`;
   return outputHtml;
 }
 
@@ -76,7 +39,18 @@ const renderTweets = function (tweets) {
 }
 
 $(document).ready(function () {
-  renderTweets(data);
+  const loadTweets = function() {
+    let url = "http://localhost:8080/tweets";
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: function (data) {
+        renderTweets(data);
+      }
+    });
+  };
+  loadTweets();
+  //renderTweets(data);
 
   $("#frm-new-tweet").submit(function (event) {
     event.preventDefault();
@@ -97,6 +71,7 @@ $(document).ready(function () {
       });
     }
   });
+
 })
 
 
